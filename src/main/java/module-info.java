49ec927fbfde6b55c2b5f52546fc915f6f23edf8
CodeDead <admin@codedead.com>
2020-02-10 01:10:28 +0100

    * Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Advanced.PassGen {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.codedead.advancedpassgen.domain.controller to javafx.fxml;
+
+    exports com.codedead.advancedpassgen;
+}
