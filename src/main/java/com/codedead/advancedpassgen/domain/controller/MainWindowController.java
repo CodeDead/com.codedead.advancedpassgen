a1c2de20b52668034f69ce6e715f31285e1e1d7e
CodeDead <admin@codedead.com>
2020-02-10 03:31:39 +0100

    * Added images to menu items * Changed default language

--- src/main/java/com/codedead/advancedpassgen/domain/controller/MainWindowController.java
+++ src/main/java/com/codedead/advancedpassgen/domain/controller/MainWindowController.java
@@ -1,5 +1,41 @@
 package com.codedead.advancedpassgen.domain.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public final class MainWindowController {
 
+    @FXML
+    private MenuItem MniHelp;
+    @FXML
+    private MenuItem MniUpdate;
+    @FXML
+    private MenuItem MniHomepage;
+    @FXML
+    private MenuItem MniLicense;
+    @FXML
+    private MenuItem MniDonate;
+    @FXML
+    private MenuItem MniAbout;
+    @FXML
+    private MenuItem MniSettings;
+    @FXML
+    private MenuItem MniExit;
+    @FXML
+    private MenuItem MniExport;
+
+    @FXML
+    public void initialize() {
+        MniExport.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/export.png"))));
+        MniExit.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/remove.png"))));
+        MniSettings.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/settings.png"))));
+        MniAbout.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/about.png"))));
+        MniDonate.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/donate.png"))));
+        MniLicense.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/license.png"))));
+        MniHomepage.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/home.png"))));
+        MniUpdate.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/update.png"))));
+        MniHelp.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/help.png"))));
+    }
 }
